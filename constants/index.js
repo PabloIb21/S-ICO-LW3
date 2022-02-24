aef2551603a3f0e77a8dbc4819e2b520fa200718
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-02-24 10:30:56 -0600

    smart contract ready

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7762Cc01b76a51Dcbb2EBDDF147F478c81aeE9dd";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
